82c5d12adf2264abbbf6d8735b6dd6747ec5e5ba
Robbie Carlton <robbie.carlton@gmail.com>
2020-01-25 11:06:23 -0700

    change timer to 10 minutes and add message when timer runs to zero

--- status/res/index.js
+++ status/res/index.js
@@ -6,7 +6,7 @@ const agentUrl = `https://${authData.holochain_agent_id}.holohost.net`
 
 const redirectIfOnline = async url => {
   try {
-    const res = await fetch(url, { mode: 'no-cors' })
+    await fetch(url, { mode: 'no-cors' })
     window.location = url
   } catch(err) {
   }
@@ -14,13 +14,26 @@ const redirectIfOnline = async url => {
 
 const $minutes = document.getElementById('minutes')
 const $seconds = document.getElementById('seconds')
+const $stepBox = document.getElementById('stepBox')
 
 const clockPad = n => n.toString().padStart(2, '0')
 
 const updateCountdown = estimate => {
   const delta = new Date(Math.max(0, estimate - Date.now()))
-  $minutes.textContent = clockPad(delta.getMinutes())
-  $seconds.textContent = clockPad(delta.getSeconds())
+  const minutes = delta.getMinutes()
+  const seconds = delta.getSeconds()
+  $minutes.textContent = clockPad(minutes)
+  $seconds.textContent = clockPad(seconds)
+  if (minutes === 0 && seconds === 0) {
+    $stepBox.innerHTML = `
+      <div class="stepLabel step5">
+        <p class="subheader-text-bookend text-white">
+          Hmm. This is taking longer than normal. Please leave this window open for another few minutes and check back later. 
+          If you're still not re-directed to your HoloPort admin panel by then, please contact <a href="https://forum.holo.host" target="_blank" class="text-white underline">customer service</a>.
+        </p>
+      </div>
+    `
+  }
 }
 
 (async () => {
@@ -28,7 +41,7 @@ const updateCountdown = estimate => {
   await fetch(authUrl)
 
   const estimate = new Date()
-  estimate.setMinutes(estimate.getMinutes() + 2)
+  estimate.setMinutes(estimate.getMinutes() + 10)
 
   setInterval(updateCountdown, 500, estimate)
   setInterval(redirectIfOnline, 5000, agentUrl)
