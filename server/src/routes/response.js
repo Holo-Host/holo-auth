7b3079357947566f88a004fa3b29a7dfec621fb2
Yegor Timoshenko <yegortimoshenko@riseup.net>
2020-01-30 12:06:58 +0000

    server: set ZeroTier device name and description to agent ID and email

--- server/src/routes/response.js
+++ server/src/routes/response.js
@@ -3,7 +3,7 @@
 import * as hmac from '../hmac'
 import { respond } from '../util'
 
-const addZeroTierMember = async address => {
+const addZeroTierMember = async (address, name, description) => {
   const apiToken = await SETTINGS.get('zerotier_central_api_token')
   const networkId = await SETTINGS.get('zerotier_network_id')
 
@@ -11,7 +11,9 @@ const addZeroTierMember = async address => {
     method: 'POST',
     headers: { authorization: `Bearer ${apiToken}` },
     body: JSON.stringify({
-      config: { authorized: true }
+      config: { authorized: true },
+      description: description,
+      name: name
     })
   })
 }
@@ -20,10 +22,10 @@ const handle = async req => {
   const { data, signature } = Object.fromEntries(req.parsedURL.searchParams)
   if (!await hmac.verify(Buffer.from(signature, 'base64'), Buffer.from(data))) { return respond(401) }
 
-  const payload = JSON.parse(data)
-  if (payload.valid_until < Date.now()) { return respond(401) }
+  const { email, holochain_agent_id, valid_until, zerotier_address } = JSON.parse(data)
+  if (valid_until < Date.now()) { return respond(401) }
 
-  return addZeroTierMember(payload.zerotier_address)
+  return addZeroTierMember(zerotier_address, holochain_agent_id, email)
 }
 
 export { handle }
