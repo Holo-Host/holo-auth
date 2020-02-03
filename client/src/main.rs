69298a01b7111fd9fba58c8ca41f3dd335edbf61
Yegor Timoshenko <yegortimoshenko@riseup.net>
2020-02-03 07:23:07 +0000

    client: log Postmark message ID, resolves #15

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,17 +1,26 @@
 use std::{env, fs};
 
 use ed25519_dalek::*;
-use failure::*;
 use hpos_config_core::{Config, public_key};
-use reqwest::Client;
 use serde::*;
+use uuid::Uuid;
 use zerotier::Identity;
+
+use failure::*;
+use tracing::*;
+use tracing_subscriber::{EnvFilter, FmtSubscriber};
 
 fn serialize_holochain_agent_id<S>(public_key: &PublicKey, serializer: S) -> Result<S::Ok, S::Error>
 where
     S: Serializer,
 {
     serializer.serialize_str(&public_key::to_base36_id(&public_key))
+}
+
+#[derive(Debug, Deserialize)]
+struct PostmarkPromise {
+    #[serde(rename = "MessageID")]
+    message_id: Uuid
 }
 
 #[derive(Debug, Serialize)]
@@ -22,7 +31,14 @@ struct Payload {
     zerotier_address: zerotier::Address
 }
 
-fn main() -> Fallible<()> {
+#[tokio::main]
+async fn main() -> Fallible<()> {
+    let subscriber = FmtSubscriber::builder()
+        .with_env_filter(EnvFilter::from_default_env())
+        .finish();
+
+    tracing::subscriber::set_global_default(subscriber)?;
+
     let config_path = env::var("HPOS_CONFIG_PATH")?;
     let config_json = fs::read(config_path)?;
     let Config::V1 { seed, settings, .. } = serde_json::from_slice(&config_json)?;
@@ -32,18 +48,22 @@ fn main() -> Fallible<()> {
 
     let zerotier_identity = Identity::read_default()?;
 
+    let client = reqwest::Client::new();
+
     let payload = Payload {
         email: settings.admin.email,
         holochain_agent_id: holochain_public_key,
         zerotier_address: zerotier_identity.address,
     };
 
-    let payload_bytes = serde_json::to_vec(&payload)?;
+    let resp = client.post("https://auth-server.holo.host/v1/challenge")
+        .json(&payload)
+        .send()
+        .await?;
 
-    Client::new()
-        .post("https://auth-server.holo.host/v1/challenge")
-        .body(payload_bytes)
-        .send()?;
+    let promise: PostmarkPromise = resp.json().await?;
+
+    info!("Postmark message ID: {}", promise.message_id);
 
     Ok(())
 }
